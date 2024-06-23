08bd7479d889563cb6c1a8cfd0e484ecbfb2066d
k-daiguji <108970032+k-daiguji@users.noreply.github.com>
2024-06-24 01:10:43 +0900

    Create attachment title and titles (#23)

---
+++ src/components/AttachmentTitles.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineProps<{ titles: string[]; titleWidth: number }>();
+</script>
+
+<template>
+  <div v-for="(title, i) in titles">
+    <div class="row">
+      <div class="title">{{ title }}</div>
+      <slot :name="i"></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.row {
+  display: flex;
+  padding: 7px 0px 7px 0px;
+}
+.title {
+  align-content: center;
+  min-width: v-bind(titleWidth + "px");
+  text-align: left;
+}
+</style>
