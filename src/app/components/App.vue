852de14c7fc0ba5fd3c9fb8e0e1a85d26fc51124
k-daiguji <hikatteru20@gmail.com>
2024-11-16 00:42:55 +0900

    Defined switch

--- src/app/components/App.vue
+++ src/app/components/App.vue
@@ -1,20 +1,20 @@
 <script setup lang="ts">
-import { ICON, Icon } from "@/base";
+import { ref } from "vue";
+
+import { ICON, Icon, Switch } from "@/base";
+
+const enabled = ref(true);
 </script>
 
 <template>
+  <Switch v-model="enabled" />
   <Icon
     :icon="ICON.CLOSE"
     @clicked="console.log('clicked1')"
   />
   <Icon
-    enabled
+    :enabled
     :icon="ICON.COPY"
     @clicked="console.log('clicked2')"
-  />
-  <Icon
-    :enabled="false"
-    :icon="ICON.COPY"
-    @clicked="console.log('clicked3')"
   />
 </template>
--- tests/app/components/App.test.ts
+++ tests/app/components/App.test.ts
@@ -1,7 +1,7 @@
 import { shallowMount } from "@vue/test-utils";
 import { describe, expect, test } from "vitest";
 
-import { ICON, Icon } from "@/base";
+import { ICON, Icon, Switch } from "@/base";
 
 import App from "@/app/components/App.vue";
 
@@ -10,14 +10,42 @@ describe("App", () => {
     const wrapper = shallowMount(App);
 
     const icons = wrapper.findAllComponents(Icon);
-
-    expect(icons).toHaveLength(3);
+    expect(icons).toHaveLength(2);
     expect(icons[0].props("enabled")).toBe(true);
     expect(icons[0].props("icon")).toBe(ICON.CLOSE);
     expect(icons[1].props("enabled")).toBe(true);
     expect(icons[1].props("icon")).toBe(ICON.COPY);
-    expect(icons[2].props("enabled")).toBe(false);
-    expect(icons[2].props("icon")).toBe(ICON.COPY);
+    wrapper.unmount();
+  });
+
+  test("Switched ON -> OFF", async () => {
+    const enabled = false;
+    const wrapper = shallowMount(App);
+
+    await wrapper.findComponent(Switch).setValue(enabled);
+
+    const icons = wrapper.findAllComponents(Icon);
+    expect(icons).toHaveLength(2);
+    expect(icons[0].props("enabled")).toBe(true);
+    expect(icons[0].props("icon")).toBe(ICON.CLOSE);
+    expect(icons[1].props("enabled")).toBe(enabled);
+    expect(icons[1].props("icon")).toBe(ICON.COPY);
+    wrapper.unmount();
+  });
+
+  test("Switched OFF -> ON", async () => {
+    const enabled = true;
+    const wrapper = shallowMount(App);
+    await wrapper.findComponent(Switch).setValue(!enabled);
+
+    await wrapper.findComponent(Switch).setValue(enabled);
+
+    const icons = wrapper.findAllComponents(Icon);
+    expect(icons).toHaveLength(2);
+    expect(icons[0].props("enabled")).toBe(true);
+    expect(icons[0].props("icon")).toBe(ICON.CLOSE);
+    expect(icons[1].props("enabled")).toBe(enabled);
+    expect(icons[1].props("icon")).toBe(ICON.COPY);
     wrapper.unmount();
   });
 });
