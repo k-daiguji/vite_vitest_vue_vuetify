a13e3e6be916c08af7da091cd67fb8224069856c
k-daiguji <hikatteru20@gmail.com>
2024-11-16 00:43:12 +0900

    Defined tab

--- tests/app/components/App.test.ts
+++ tests/app/components/App.test.ts
@@ -1,51 +1,23 @@
 import { shallowMount } from "@vue/test-utils";
 import { describe, expect, test } from "vitest";
+import { shallowRef } from "vue";
 
-import { ICON, Icon, Switch } from "@/base";
+import { Tabs } from "@/base";
 
 import App from "@/app/components/App.vue";
+import Sub from "@/app/components/Sub.vue";
 
 describe("App", () => {
   test("Mounted", () => {
     const wrapper = shallowMount(App);
 
-    const icons = wrapper.findAllComponents(Icon);
-    expect(icons).toHaveLength(2);
-    expect(icons[0].props("enabled")).toBe(true);
-    expect(icons[0].props("icon")).toBe(ICON.CLOSE);
-    expect(icons[1].props("enabled")).toBe(true);
-    expect(icons[1].props("icon")).toBe(ICON.COPY);
-    wrapper.unmount();
-  });
-
-  test("Switched ON -> OFF", async () => {
-    const enabled = false;
-    const wrapper = shallowMount(App);
-
-    await wrapper.findComponent(Switch).setValue(enabled);
-
-    const icons = wrapper.findAllComponents(Icon);
-    expect(icons).toHaveLength(2);
-    expect(icons[0].props("enabled")).toBe(true);
-    expect(icons[0].props("icon")).toBe(ICON.CLOSE);
-    expect(icons[1].props("enabled")).toBe(enabled);
-    expect(icons[1].props("icon")).toBe(ICON.COPY);
-    wrapper.unmount();
-  });
-
-  test("Switched OFF -> ON", async () => {
-    const enabled = true;
-    const wrapper = shallowMount(App);
-    await wrapper.findComponent(Switch).setValue(!enabled);
-
-    await wrapper.findComponent(Switch).setValue(enabled);
-
-    const icons = wrapper.findAllComponents(Icon);
-    expect(icons).toHaveLength(2);
-    expect(icons[0].props("enabled")).toBe(true);
-    expect(icons[0].props("icon")).toBe(ICON.CLOSE);
-    expect(icons[1].props("enabled")).toBe(enabled);
-    expect(icons[1].props("icon")).toBe(ICON.COPY);
+    const tabs = wrapper.findComponent(Tabs);
+    expect(tabs.props("modelValue")).toStrictEqual([
+      { name: "Aaa", component: Sub },
+      { name: "bBb", component: Sub },
+    ]);
+    expect(tabs.props("isFixedLength")).toBe(true);
+    expect(tabs.props("isShowAnimation")).toBe(true);
     wrapper.unmount();
   });
 });
