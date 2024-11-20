5acb9d5acb8d84eadbe9e90107af2b570ad0f2c8
k-daiguji <hikatteru20@gmail.com>
2024-11-20 23:54:50 +0900

    Defined sections

--- src/app/components/App.vue
+++ src/app/components/App.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
 import { ref } from "vue";
 
-import { ICON, Icon, Switch } from "@/base";
+import { ICON, Icon, Section, Sections, Switch } from "@/base";
 
 const enabled = ref(true);
+const activeTitle = ref("Title 1");
+const sections = [
+  { title: "Title 1", body: 1 },
+  { title: "Title 2", body: 2 },
+  { title: "Title 3", body: 3 },
+];
 </script>
 
 <template>
@@ -17,4 +23,18 @@ const enabled = ref(true);
     :icon="ICON.COPY"
     @clicked="console.log('clicked2')"
   />
+  <Sections
+    class="mt-5"
+    :active-title
+    :enabled
+    @switched="activeTitle = $event"
+  >
+    <Section
+      v-for="{ title, body } in sections"
+      :key="title"
+      :title
+    >
+      <div>{{ body }}</div>
+    </Section>
+  </Sections>
 </template>
