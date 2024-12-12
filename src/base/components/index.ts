b22b5060f2fd0ce8bafa1bd4d886b44a6268b63b
k-daiguji <hikatteru20@gmail.com>
2024-12-13 00:35:23 +0900

    Defined tables

--- src/base/components/index.ts
+++ src/base/components/index.ts
@@ -1,6 +1,7 @@
 import Icon from "@/base/components/Icon.vue";
 import List from "@/base/components/List.vue";
 import Switch from "@/base/components/Switch.vue";
+import Table from "@/base/components/Table.vue";
 import Tabs from "@/base/components/Tabs.vue";
 
-export { Icon, List, Switch, Tabs };
+export { Icon, List, Switch, Table, Tabs };
