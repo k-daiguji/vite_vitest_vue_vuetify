93d1756d6059de59694f4fbe7be83eb56b82188d
k-daiguji <hikatteru20@gmail.com>
2025-04-07 01:38:07 +0900

    Add unittest

--- tests/components/HelpArea.test.ts
+++ tests/components/HelpArea.test.ts
@@ -1,23 +1,75 @@
-import { shallowMount } from "@vue/test-utils";
+import { mount } from "@vue/test-utils";
 import { test } from "vitest";
+import { defineComponent } from "vue";
 
-import BaseButton from "@/app/components/BaseButton.vue";
 import HelpArea from "@/app/components/HelpArea.vue";
 import { themeColor } from "@/app/constants/themeColor";
 
-test("Mounted", ({ expect }) => {
-  using wrapper = shallowMount(HelpArea);
+const dummyButton = defineComponent({
+  props: {
+    themeColor: {
+      required: true,
+      type: String,
+    },
+    enabled: {
+      default: true,
+      type: Boolean,
+    },
+    rounded: {
+      default: "xs",
+      type: String,
+    },
+    variant: {
+      default: "text",
+      type: String,
+    },
+  },
+  template: "<div><slot/></div>",
+});
+const dummyIcon = defineComponent({
+  props: {
+    icon: {
+      required: true,
+      type: String,
+    },
+    enabled: {
+      default: true,
+      type: Boolean,
+    },
+    embedded: {
+      default: false,
+      type: Boolean,
+    },
+  },
+  template: "<div><slot/></div>",
+});
 
-  const buttons = wrapper.findAllComponents(BaseButton);
+test("Mounted", ({ expect }) => {
+  using wrapper = mount(HelpArea, {
+    global: {
+      stubs: {
+        Button: dummyButton,
+        Icon: dummyIcon,
+      },
+    },
+  });
+
+  const buttons = wrapper.findAllComponents(dummyButton);
   expect(buttons.length).toBe(2);
-  const [cancel, ok] = buttons;
-  if (cancel && ok) {
-    expect(cancel.props("text")).toBe("Cancel");
-    expect(cancel.props("themeColor")).toBe(themeColor.primary);
-    expect(cancel.props("rounded")).toBe("xl");
-    expect(ok.props("text")).toBe("OK");
-    expect(ok.props("themeColor")).toBe(themeColor.primary);
-    expect(ok.props("rounded")).toBe("xl");
+  const [a, b] = buttons;
+  if (a && b) {
+    expect(a.props("themeColor")).toBe(themeColor.primary);
+    expect(a.props("rounded")).toBe("xl");
+    expect(a.text()).toBe("Home");
+    const aIcon = a.findComponent(dummyIcon);
+    expect(aIcon.props("embedded")).toBe(true);
+    expect(aIcon.props("icon")).toBe("home");
+    expect(b.props("themeColor")).toBe(themeColor.primary);
+    expect(b.props("rounded")).toBe("xl");
+    expect(b.text()).toBe("Home");
+    const bIcon = b.findComponent(dummyIcon);
+    expect(bIcon.props("embedded")).toBe(true);
+    expect(bIcon.props("icon")).toBe("home");
   } else {
     expect.fail("Buttons not found");
   }
