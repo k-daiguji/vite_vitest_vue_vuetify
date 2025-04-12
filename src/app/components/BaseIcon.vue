771d907ae85e94f298f80ed197c21b960fbaa886
k-daiguji <108970032+k-daiguji@users.noreply.github.com>
2025-04-13 02:17:34 +0900

    Add icon component (#73)

---
+++ src/app/components/BaseIcon.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import type { icon } from "@/app/constants/icon";
+import type { Theme } from "@/app/types/color";
+
+const { enabled = true } = defineProps<{
+  icon: (typeof icon)[keyof typeof icon];
+  theme: Theme;
+
+  enabled?: boolean;
+}>();
+</script>
+
+<template>
+  <span
+    class="icon"
+    :class="icon, theme, { enabled }"
+  ></span>
+</template>
+
+<style scoped>
+.icon {
+  align-items: center;
+  display: flex;
+  font-size: 32px;
+  height: 32px;
+  width: 32px;
+}
+</style>
---
+++ src/app/constants/icon.ts
@@ -0,0 +1,4 @@
+export const icon = {
+  lock: "mdi mdi-lock",
+  unlock: "mdi mdi-lock-open",
+} as const;
