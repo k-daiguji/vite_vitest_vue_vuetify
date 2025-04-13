9d29986552ac5547f85b6d98d9373c25d1465dc3
k-daiguji <108970032+k-daiguji@users.noreply.github.com>
2025-04-13 16:04:09 +0900

    Rename views (#91)

---
+++ src/app/views/AreaIntra.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+import BaseButton from "@/app/components/BaseButton.vue";
+import { theme } from "@/app/constants/color";
+
+const state = ref(false);
+</script>
+
+<template>
+  <div>
+    <p id="state">State: {{ state }}</p>
+    <BaseButton
+      id="off-button"
+      text="OFF"
+      :theme="theme.primary"
+      rounded="xl"
+      @click="state = false"
+    />
+    <BaseButton
+      id="on-button"
+      text="ON"
+      :theme="theme.primary"
+      rounded="xl"
+      @click="state = true"
+    />
+  </div>
+</template>
