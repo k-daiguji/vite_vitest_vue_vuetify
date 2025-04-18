26f61511e579e73d6f74c0376311ffd072551bcd
k-daiguji <108970032+k-daiguji@users.noreply.github.com>
2025-04-19 00:12:13 +0900

    Replace button (#103)

--- tests/components/BaseButton.test.ts
+++ tests/components/BaseButton.test.ts
@@ -1,59 +1,53 @@
-import type { VueWrapper } from "@vue/test-utils";
-import { mount } from "@vue/test-utils";
-import { test } from "vitest";
-import { createVuetify } from "vuetify";
-import { VBtn } from "vuetify/components";
+import { shallowMount } from "@vue/test-utils";
+import { describe, test } from "vitest";
 
 import BaseButton from "@/app/components/BaseButton.vue";
-import { theme } from "@/app/constants/color";
 
-const vuetify = createVuetify({ components: { VBtn } });
+const slots = { default: "dummyText" };
 
-const customTest = test.extend<{ wrapper: VueWrapper }>({
-  // biome-ignore lint/correctness/noEmptyPattern: <explanation>
-  wrapper: async ({}, use) => {
-    using wrapper = mount(BaseButton, {
-      props: {},
-      slots: { default: "dummyText" },
-      global: {
-        plugins: [vuetify],
-      },
-    });
-    await use(wrapper);
-  },
+test("Mounted", ({ expect }) => {
+  using wrapper = shallowMount(BaseButton, { slots });
+
+  const testee = wrapper.find("button");
+  expect(testee.attributes("disabled")).toBeUndefined();
+  expect(testee.classes()).toStrictEqual(["button", "rounded-full"]);
+  expect(testee.text()).toBe(slots.default);
 });
 
-customTest("Mounted", ({ expect, wrapper }) => {
-  const button = wrapper.findComponent(VBtn);
-  const classes = button.classes();
-  expect(classes).toContain("text-none");
-  const props = button.props();
-  expect(props.disabled).toBe(false);
-  expect(props.ripple).toBe(false);
-  expect(props.rounded).toBe("xl");
-  expect(props.variant).toBe("flat");
-  expect(button.text()).toBe("dummyText");
+describe("Changed props", () => {
+  test("enabled", async ({ expect }) => {
+    using wrapper = shallowMount(BaseButton, { slots });
+
+    await wrapper.setProps({ enabled: false });
+
+    const testee = wrapper.find("button");
+    expect(testee.attributes("disabled")).toBeDefined();
+
+    await wrapper.setProps({ enabled: true });
+
+    expect(testee.attributes("disabled")).toBeUndefined();
+  });
+
+  test("rounded", async ({ expect }) => {
+    using wrapper = shallowMount(BaseButton, { slots });
+
+    await wrapper.setProps({ rounded: "0" });
+
+    const testee = wrapper.find("button");
+    expect(testee.classes("rounded-full")).toBe(false);
+
+    await wrapper.setProps({ rounded: undefined });
+
+    expect(testee.classes("rounded-full")).toBe(true);
+  });
 });
 
-customTest("setProps(enabled)", async ({ expect, wrapper }) => {
-  await wrapper.setProps({ enabled: false });
+test("When the button is pressed, an event is triggered.", async ({
+  expect,
+}) => {
+  using wrapper = shallowMount(BaseButton, { slots });
 
-  const button = wrapper.findComponent(VBtn);
-  expect(button.props("disabled")).toBe(true);
+  await wrapper.find("button").trigger("click");
 
-  await wrapper.setProps({ enabled: true });
-
-  expect(button.props("disabled")).toBe(false);
-});
-
-customTest("setProps(rounded)", async ({ expect, wrapper }) => {
-  await wrapper.setProps({ rounded: "0" });
-
-  expect(wrapper.findComponent(VBtn).props("rounded")).toBe("0");
-});
-
-customTest("setProps(variant)", async ({ expect, wrapper }) => {
-  await wrapper.setProps({ variant: "text" });
-
-  expect(wrapper.findComponent(VBtn).props("variant")).toBe("text");
+  expect(wrapper.emitted("click")).toStrictEqual([[]]);
 });
