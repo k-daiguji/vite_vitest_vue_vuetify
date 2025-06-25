f05d030c638b9bb50bfef702f6d471d75d674893
k-daiguji <108970032+k-daiguji@users.noreply.github.com>
2025-06-25 21:46:04 +0900

    Fixed type error (#136)

--- src/app/views/AreaIntra.vue
+++ src/app/views/AreaIntra.vue
@@ -7,31 +7,73 @@ import BaseTable from "@/app/components/BaseTable.vue";
 import { useSection } from "@/app/composables/useAccordion";
 import { theme } from "@/app/constants/color";
 import { NumberPixel } from "@/app/models/pixelNumber";
+import type { Table } from "@/app/types/table";
 import Dummy from "@/app/views/DummyComponent.vue";
 
 const state = ref(false);
 
 const maxHeight = NumberPixel.create(200);
+
 const table = {
   headers: ["", "Header 2", "Header 3", "Header 4"],
   bodies: [
-    [{ cell: "test1", indent: 0 }, { cell: 1 }, { cell: 2 }, { cell: 3 }],
-    [{ cell: "test1-1", indent: 2 }, { cell: 4 }, { cell: 5 }, { cell: 6 }],
-    [{ cell: "test1-1-1", indent: 4 }, { cell: 7 }, { cell: 8 }, { cell: 9 }],
+    [{ cell: "test1", indent: 0 }, { cell: "1" }, { cell: "2" }, { cell: "3" }],
+    [
+      { cell: "test1-1", indent: 2 },
+      { cell: "4" },
+      { cell: "5" },
+      { cell: "6" },
+    ],
+    [
+      { cell: "test1-1-1", indent: 4 },
+      { cell: "7" },
+      { cell: "8" },
+      { cell: "9" },
+    ],
     [
       { cell: "test1-1-2", indent: 4 },
-      { cell: 10 },
-      { cell: 11 },
-      { cell: 12 },
+      { cell: "10" },
+      { cell: "11" },
+      { cell: "12" },
     ],
-    [{ cell: "test2", indent: 0 }, { cell: 13 }, { cell: 14 }, { cell: 15 }],
-    [{ cell: "test2-1", indent: 2 }, { cell: 16 }, { cell: 17 }, { cell: 18 }],
-    [{ cell: "test2-2", indent: 2 }, { cell: 19 }, { cell: 20 }, { cell: 21 }],
-    [{ cell: "test2-3", indent: 2 }, { cell: 22 }, { cell: 23 }, { cell: 24 }],
-    [{ cell: "test3", indent: 0 }, { cell: 25 }, { cell: 26 }, { cell: 27 }],
-    [{ cell: "test4", indent: 0 }, { cell: 28 }, { cell: 29 }, { cell: 30 }],
+    [
+      { cell: "test2", indent: 0 },
+      { cell: "13" },
+      { cell: "14" },
+      { cell: "15" },
+    ],
+    [
+      { cell: "test2-1", indent: 2 },
+      { cell: "16" },
+      { cell: "17" },
+      { cell: "18" },
+    ],
+    [
+      { cell: "test2-2", indent: 2 },
+      { cell: "19" },
+      { cell: "20" },
+      { cell: "21" },
+    ],
+    [
+      { cell: "test2-3", indent: 2 },
+      { cell: "22" },
+      { cell: "23" },
+      { cell: "24" },
+    ],
+    [
+      { cell: "test3", indent: 0 },
+      { cell: "25" },
+      { cell: "26" },
+      { cell: "27" },
+    ],
+    [
+      { cell: "test4", indent: 0 },
+      { cell: "28" },
+      { cell: "29" },
+      { cell: "30" },
+    ],
   ],
-};
+} satisfies Table;
 
 const title = ref("");
 const a = useSection(
