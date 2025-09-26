10c090434128c37be393b01e982ea5349c33ba6f
k-daiguji <hikatteru20@gmail.com>
2025-09-27 01:50:22 +0900

    Rename client to editor

--- editor/src/renderer.ts
+++ editor/src/renderer.ts
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 
-import App from "@/client/src/views/App.vue";
+import App from "@/editor/src/views/App.vue";
 import { vuetify } from "@/plugins/vuetify";
 
 createApp(App).use(vuetify).mount("#app");
